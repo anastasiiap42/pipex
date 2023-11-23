a9a5c04b0bd036070e77798e63136556e73fd888
Anastasiia <apashkov@c2r4p2.42vienna.com>
2023-11-23 17:58:02 +0100

    the newest

--- pipex.c
+++ pipex.c
@@ -6,18 +6,20 @@
 /*   By: apashkov <apashkov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 11:49:59 by apashkov          #+#    #+#             */
-/*   Updated: 2023/11/21 19:39:04 by apashkov         ###   ########.fr       */
+/*   Updated: 2023/11/23 17:56:51 by apashkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-/* void	child1_process(int file1, char *cmd1)
+void	child_process(int file1)
 {
-	if (dup2(file1, STDIN_FILENO) < 0)
+	if (dup2(file1, 0) < 0)
 		return (perror("Dup2: "));
-	dup2(end[1], );
+	if (dup2(end[1], 1) < 0)
+		return (perror("Dup2: "));
 	close(end[0]);
+	close (file1);
 	exit(1);
 }
 
@@ -25,15 +27,16 @@ void	parent_process(int file2)
 {
 	int	status;
 
-	waitpid(-1, &status, 0);
-	if (dup2(file2, ) < 0)
+	if (dup2(end[0], 0) < 0)
 		return (perror("Dup2: "));
-	dup2(end[0], );
+	if (dup2(file2, 1) < 0)
+		return (perror("Dup2: "));
 	close(end[1]);
+	close(file2);
 	exit(1);
-} */
+}
 
-/* void	pipex(int file1, int file2)
+void	pipex(int file1, int file2)
 {
 	int		end[2];
 	pid_t	child1;
@@ -42,35 +45,34 @@ void	parent_process(int file2)
 
 	if (pipe(end) == -1)
 		return (perror("Pipe: "));
-	child1 = fork();
-	if (child1 < 0)
-		return (perror("Fork1: "));
-	if (child1 == 0)
-		//child1_process(file1, char *cmd1);
-	child2 = fork();
-	if (child2 < 0)
-		return (perror("Fork2: "));
-	if (child2 == 0)
-		//child2_process(file2, char *cmd2);
-	waitpid(child1, &status, 0);
-	waitpid(child2, &status, 0);
-} */
-
-void print_split(char **argv) 
-{
-	char **result = ft_split(argv[1], argv[2][0]);
-
-	for (int i = 0; result[i]; i++)
+	child = fork();
+	if (child < 0)
+		return (perror("Fork: "));
+	if (child == 0)
+		child_process(file1);
+	else
 	{
-		printf("[%s]\n", result[i]);
+		waitpid(child, &status, 0);
+		parent_process(file2);
 	}
-	free_array(result);
 }
+
+/* void print_split(char **argv) 
+{
+	int i = 0;
+	while (argv[i])
+	{
+		printf("[%s]\n", argv[i]);
+		i++;
+	}
+	printf("[%s]\n", argv[i]);
+	free_array(argv);
+} */
 
 int	main(int argc, char** argv, char **envp)
 {
-	/* int	infile;
-	int outfile; */
+	int	infile;
+	int outfile;
 	char *the_path;
 	char **poss_paths;
 	int	i;
@@ -85,13 +87,9 @@ int	main(int argc, char** argv, char **envp)
 	poss_paths = ft_split(the_path, ':');
 	if (!poss_paths)
 		return(perror("Split: "), 1);
-	print_split(poss_paths);
-	/* if (argc)
-	{
-		infile = open(argv[1], O_RDONLY);
-		outfile = open(argv[4], O_RDWR);
-		if (infile < 0 || outfile < 0)
-			return(perror("Open: "), 1);
-		pipex(file1, file2);
-	} */
+	infile = open(argv[1], O_RDONLY);
+	outfile = open(argv[4], O_RDWR);
+	if (infile < 0 || outfile < 0)
+		return(perror("Open: "), 1);
+	pipex(file1, file2);
 }
